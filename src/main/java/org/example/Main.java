dc8303a941ba3568f2554312102fa09ef3dc993a
Kaczmarek076 <sebkac01@easv365.dk>
2025-11-19 13:22:31 +0100

    Bit mining date 19/11 time 11:20

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,6 @@ public class Main {
 
         Factory bitMining = new Factory();
 
-        /*
-        for (int i = 0; i < 100; i++) {
-            boolean temp;
-            temp = bitMining.produceRandomBit();
-
-            System.out.println(temp);
-        }
-         */
-
         bitMining.produceByte(6 );
 
 
